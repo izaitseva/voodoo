2deb2b030b6863f0cd023e4e4242d4fe648e2a47
Iryna Zaitseva <zaitseva02@gmail.com>
2023-09-29 13:29:18 +0300

    few syntax changes

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const URL = "https://voodoo-sandbox.myshopify.com/products.json?limit=24";
 
 async function fetchProducts() {
-  const cardPromise = await fetch(URL);
-  const result = await cardPromise.json();
+  const cartPromise = await fetch(URL);
+  const result = await cartPromise.json();
 
   console.log(result);
 
-  const cardList = document.getElementById("card-list");
+  const cartList = document.getElementById("cart-list");
 
   
   result.products.forEach((product) => {
     const li = document.createElement("li");
-    li.classList.add("card-item");
+    li.classList.add("cart-item");
     console.log(product);
 
     const Image = product.images.length > 0 ? product.images[0].src : "/images/no-image.png";
@@ -19,20 +19,20 @@ async function fetchProducts() {
             <div class="info-container">
             <p class="info-text">used</p>
             </div>
-            <img class="card-img" src="${Image}" alt="#"/>
-            <div class="card-desc-container">
-            <div class="card-prod-desc">
+            <img class="cart-img" src="${Image}" alt="#"/>
+            <div class="cart-desc-container">
+            <div class="cart-prod-desc">
               <p class="product-name">${product.title}</p>
               <p class="product-name">${product.variants[0].price}</p>
             </div>
-            <div class="card-condition-desc">
+            <div class="cart-condition-desc">
               <p class="product-condition">Condition</p>
               <p class="product-used">Slightly used</p>
             </div>
           </div>
-          <button class="add-to-card-btn">Add to card</button>
+          <button class="add-to-cart-btn">Add to cart</button>
     `;
-    cardList.appendChild(li);
+    cartList.appendChild(li);
   });
 }
 
