15a74e0a412893c2bcf90387afe8af98e266a9a1
Iryna Zaitseva <zaitseva02@gmail.com>
2023-09-29 13:17:27 +0300

    get image from the API

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const URL = "https://voodoo-sandbox.myshopify.com/products.json?limit=24";
 
-export async function fetchProducts() {
+async function fetchProducts() {
   const cardPromise = await fetch(URL);
   const result = await cardPromise.json();
 
@@ -8,15 +8,18 @@ export async function fetchProducts() {
 
   const cardList = document.getElementById("card-list");
 
+  
   result.products.forEach((product) => {
     const li = document.createElement("li");
     li.classList.add("card-item");
     console.log(product);
+
+    const Image = product.images.length > 0 ? product.images[0].src : "/images/no-image.png";
     li.innerHTML = `
             <div class="info-container">
             <p class="info-text">used</p>
             </div>
-            <img class="card-img" src="/images/no-image.png" alt="#"/>
+            <img class="card-img" src="${Image}" alt="#"/>
             <div class="card-desc-container">
             <div class="card-prod-desc">
               <p class="product-name">${product.title}</p>
